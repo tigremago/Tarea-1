71f78cc7ed4ef705bfdf5319154e0e153dc69923
tigremago <ra.yanjari@duocuc.cl>
2026-03-31 20:02:29 -0300

    <<

--- src/main/java/com/example/demo/Service/PeliculaService.java
+++ src/main/java/com/example/demo/Service/PeliculaService.java
@@ -1,5 +1,45 @@
 package com.example.demo.Service;
 
+import com.example.demo.Model.Pelicula;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Service
 public class PeliculaService {
 
+    List<Pelicula> peliculas = new ArrayList<>();
+
+    // listar por peliculas
+    public List<Pelicula> listar(){
+        return peliculas;
+    }
+
+    // agregar pelicula
+
+    public String agregar(Pelicula pelicula){
+
+        // validacion simple
+        if(pelicula.getNombre() == null || pelicula.getNombre().isEmpty()){
+            return "el nombre es obligatorio";
+        }
+
+        peliculas.add(pelicula);
+        return "pelicula agregada correctamente";
+    }
+
+    // eliminar pelicula
+    public String eliminar(String nombre){
+        for (Pelicula p : peliculas){
+            if(p.getNombre().equalsIgnoreCase(nombre)){
+                peliculas.remove(p);
+                return "pelicula elimindada";
+            }
+        }
+        return "pelicula no encontrada";
+
+    }
+
 }
+
